226adda65e5da4eecc6eafa0201486f64815fdc0
Catherine Tan <llt3@sfu.ca>
2013-02-16 15:47:17 -0800

    set up node server

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade'); 
+app.set('view options', { layout: false });
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', function(req, res){
+  // res.send('hello world');
+  res.render('index', function(err, rendered){
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.end(rendered);  
+  })
+});
+
+app.listen(3000);
